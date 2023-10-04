7684eae0707c70167db8b45f20c2d64fcfa12b8c
Saakira22 <145532751+Saakira22@users.noreply.github.com>
2023-10-04 21:44:46 +0530

    Comments added to classroom.java

--- virtualclassroom/src/virtualclassroom/Classroom.java
+++ virtualclassroom/src/virtualclassroom/Classroom.java
@@ -6,39 +6,34 @@ import java.util.List;
 class Classroom {
 
 	// "Private" access modifier is used for maintaining the sensitive data
+	
 	private String name;
 	private List<String> students;
 	private List<String> assignments;
+	
 
 	// Constructor for Classroom Class
+	
 	public Classroom(String name) {
 		this.name = name;
 		this.students = new ArrayList<>();
 		this.assignments = new ArrayList<>();
 	}
-
+	
+	
+        // Getters for private fields
+	
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public List<String> getStudents() {
 		return students;
 	}
-
-	public void setStudents(List<String> students) {
-		this.students = students;
-	}
-
+	
 	public List<String> getAssignments() {
 		return assignments;
 	}
 
-	public void setAssignments(List<String> assignments) {
-		this.assignments = assignments;
-	}
-
+	
 }
